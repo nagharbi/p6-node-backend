bc4deacc2ab39e26861b30c19e0f05f137137d6b
Nouha Gharbi <na.gharbi.94@gmail.com>
2022-09-12 13:03:16 +0200

    modifier les champs

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.get('/api/stuff/:id', (req,res,next) => {
     .then(Thing => res.status(200).json(Thing))
     .catch(error =>res.status(404).json({error}));
 });
+// modifier un Thing existant 
+app.put('/api/stuff/:id',(req,res,next)=>{
+    Thing.updateOne({_id:req.params.id} , {...req.body,_id:req.params.id})
+    .then(()=>res.status(200).json({message:'objet modifier'}))
+    .catch(error =>res.status(400).json({error}));
+});
 
 // La route GET all
 // find pour recupérer tous thing
