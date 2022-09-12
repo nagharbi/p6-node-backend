93210a1ed106d814ac6c5d506a8468b439ec34fb
Nouha Gharbi <na.gharbi.94@gmail.com>
2022-09-12 12:42:15 +0200

    Enregistrez et récupérez des données (methode get)

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
 });
 // enregistrer des objets dans base se donnée
 //L'opérateur spread ... est utilisé pour faire une copie de tous les éléments de req.body
+//save() qui enregistre Thing dans la base de donnée
 app.post('/api/stuff', (req, res, next) => {
     delete req.body._id;
     const thing = new Thing({
@@ -30,27 +31,19 @@ app.post('/api/stuff', (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
 });
 
-// La route GET /api/stuff
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+// la route "GET ONE"
+app.get('/api/stuff/:id', (req,res,next) => {
+    Thing.findOne ({_id:req.params.id})
+    .then(Thing => res.status(200).json(Thing))
+    .catch(error =>res.status(404).json({error}));
+});
+
+// La route GET all
+// find pour recupérer tous thing
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+    .then(Thing => res.status(200).json(Thing))
+    .catch(error =>res.status(400).json({error}));
 });
 
 module.exports = app;
