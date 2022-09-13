000a1a0c07cdcd222e6dfe5eb380dd9fc629c3b9
Nouha Gharbi <na.gharbi.94@gmail.com>
2022-09-13 22:18:08 +0200

    creer la fonction signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,22 @@
 
+const bcrypt = require('bcrypt');
+const { use } = require('../app');
+
+const user = require('../models/user');
 
 
 exports.signup = (req, res, next) => {
-
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user = new user({
+            email : req.body.email,
+            password: hahd
+        });
+        user.save()
+        .then(() => res.status(201).json({message : 'utilisateur créé !'}))
+        .catch(Error => res.status(400).json({ Error}));
+    })
+    .catch(Error => res.status(500).json({Error}));
 };
 
 
