0035a1e2185aaf47c06687dc2149a8a509bf54ee
Nouha Gharbi <na.gharbi.94@gmail.com>
2022-09-14 12:22:54 +0200

    ajouter return eu login

--- controllers/user.js
+++ controllers/user.js
@@ -22,12 +22,12 @@ exports.login = (req, res, next) => {
     User.findOne ({email: req.body.email})
     .then(user => {
         if (user === null) {
-            res.status(401).json({message:'paire identifiant / mot de passe incorrecte'});
+           return res.status(401).json({message:'paire identifiant / mot de passe incorrecte'});
         } else {
             bcrypt.compare(req.body.password, user.password)
             .then(valid =>{
                 if (!valid) {
-                    res.status(401).json({message:'paire identifiant / mot de passe incorrecte'})
+                   return res.status(401).json({message:'paire identifiant / mot de passe incorrecte'})
                 } else {
                     res.status(200).json({
                         userId : user._id,
